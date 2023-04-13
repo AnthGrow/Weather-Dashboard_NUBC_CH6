35e6ee00cbee762aa83e0ba62d5e54d34857e462
AnthGrow <122587083+AnthGrow@users.noreply.github.com>
2023-04-13 18:35:40 -0500

    worked with Pat to try to repair.  Retooled the Fetch and commented out several items that weren't working

--- Assets/script.js
+++ Assets/script.js
@@ -1,96 +1,104 @@
-var userFormEl = document.querySelector('#user-form');
-var languageButtonsEl = document.querySelector('#language-buttons');
-var locationEl = document.querySelector('#location');
-var repoContainerEl = document.querySelector('#repos-container');
-var repoSearchTerm = document.querySelector('#repo-search-term');
+var userFormEl = document.querySelector("#user-form");
+var languageButtonsEl = document.querySelector("#language-buttons");
+var locationEl = document.querySelector("#location");
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
 
 var formSubmitHandler = function (event) {
   event.preventDefault();
 
-  var location = nameInputEl.value.trim();
+  var location = locationEl.value.trim();
+  console.log(location);
 
-  if (username) {
-    getUserRepos(username);
+  var apiUrl =
+    "http://api.openweathermap.org/geo/1.0/direct?appid=2c69c6f3e98dcbf764bb60ef32982230&q=" +
+    location;
 
-    repoContainerEl.textContent = '';
-    nameInputEl.value = '';
-  } else {
-    alert('Please enter a GitHub username');
-  }
+  fetch(apiUrl);
+
+  // if (username) {
+  //   getUserRepos(username);
+
+  //   repoContainerEl.textContent = '';
+  //   nameInputEl.value = '';
+  // } else {
+  //   alert('Please enter a GitHub username');
+  // }
 };
 
 var buttonClickHandler = function (event) {
-  var language = event.target.getAttribute('data-language');
+  var language = event.target.getAttribute("data-language");
 
   if (language) {
     getFeaturedRepos(language);
 
-    repoContainerEl.textContent = '';
+    repoContainerEl.textContent = "";
   }
 };
 
 var getLocation = function (locationEl) {
-  var apiUrl = 'http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={2c69c6f3e98dcbf764bb60ef32982230'
-console.log
-}
+  var apiUrl =
+    "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={2c69c6f3e98dcbf764bb60ef32982230";
 
-  fetch(apiUrl) {
+  fetch(apiUrl)
     .then(function (response) {
       if (response.ok) {
         response.json().then(function (name) {
           displayRepos(name, user);
         });
       } else {
-        alert('Error: ' + response.statusText);
+        alert("Error: " + response.statusText);
       }
     })
     .catch(function (error) {
-      alert('Unable to connect to Openweathermap');
+      alert("Unable to connect to Openweathermap");
     });
 };
 
-var getWeather = function (weatherCurrent) {
-  var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={2c69c6f3e98dcbf764bb60ef32982230'
-}
+// var getWeather = function (weatherCurrent) {
+//   var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={2c69c6f3e98dcbf764bb60ef32982230'
+// }
 
-  fetch(apiUrl).then(function (response) {
-    if (response.ok) {
-      response.json().then(function (data) {
-        displayRepos(data.items, language);
-      });
-    } else {
-      alert('Error: ' + response.statusText);
-    }
-  });
-
+//   fetch(apiUrl).then(function (response) {
+//     if (response.ok) {
+//       response.json().then(function (data) {
+//         displayRepos(data.items, language);
+//       });
+//     } else {
+//       alert('Error: ' + response.statusText);
+//     }
+//   });
 
 var displayRepos = function (repos, searchTerm) {
   if (repos.length === 0) {
-    repoContainerEl.textContent = 'No repositories found.';
+    repoContainerEl.textContent = "No repositories found.";
     return;
   }
 
   repoSearchTerm.textContent = searchTerm;
 
   for (var i = 0; i < repos.length; i++) {
-    var repoName = repos[i].owner.login + '/' + repos[i].name;
+    var repoName = repos[i].owner.login + "/" + repos[i].name;
 
-    var repoEl = document.createElement('div');
-    repoEl.classList = 'list-item flex-row justify-space-between align-center';
+    var repoEl = document.createElement("div");
+    repoEl.classList = "list-item flex-row justify-space-between align-center";
 
-    var titleEl = document.createElement('span');
+    var titleEl = document.createElement("span");
     titleEl.textContent = repoName;
 
     repoEl.appendChild(titleEl);
 
-    var statusEl = document.createElement('span');
-    statusEl.classList = 'flex-row align-center';
+    var statusEl = document.createElement("span");
+    statusEl.classList = "flex-row align-center";
 
     if (repos[i].open_issues_count > 0) {
       statusEl.innerHTML =
-        "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + ' issue(s)';
+        "<i class='fas fa-times status-icon icon-danger'></i>" +
+        repos[i].open_issues_count +
+        " issue(s)";
     } else {
-      statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+      statusEl.innerHTML =
+        "<i class='fas fa-check-square status-icon icon-success'></i>";
     }
 
     repoEl.appendChild(statusEl);
@@ -99,5 +107,5 @@ var displayRepos = function (repos, searchTerm) {
   }
 };
 
-userFormEl.addEventListener('submit', formSubmitHandler);
-languageButtonsEl.addEventListener('click', buttonClickHandler);
+userFormEl.addEventListener("submit", formSubmitHandler);
+// languageButtonsEl.addEventListener('click', buttonClickHandler);
